d9afafaecf76814092b3246febe939b6ed17f307
Amir Salimi <85235736+amirsalimi92@users.noreply.github.com>
2024-05-15 11:52:56 +0200

    Step1_No Data Binding

--- webapp/scripts/index.js
+++ webapp/scripts/index.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+sap.ui.require(["sap/m/Text"], function (Text) {
+  "use strict";
+
+  // Attach an anonymous function to the SAPUI5 'init' event
+
+  // this.getView().getModel()
+  sap.ui.getCore().attachInit(function () {
+    // Create a text UI element that displays a hardcoded text string
+    new Text({
+      text: "Hi my name is Amir",
+    }).placeAt("content");
+  });
+});
