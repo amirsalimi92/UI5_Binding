535250a9dde6ac99733e7edc368f71eddc38111f
Amir Salimi <85235736+amirsalimi92@users.noreply.github.com>
2024-05-16 11:04:19 +0200

    Step2_Connecting an extra json

--- webapp/scripts/index.js
+++ webapp/scripts/index.js
@@ -1,15 +1,34 @@
 console.log("test");
 
-sap.ui.require(["sap/m/Text"], function (Text) {
-  "use strict";
+sap.ui.require(
+  ["sap/ui/model/json/JSONModel", "sap/m/Text"],
+  function (JSONModel, Text) {
+    "use strict";
 
-  // Attach an anonymous function to the SAPUI5 'init' event
+    // Attach an anonymous function to the SAPUI5 'init' event
 
-  // this.getView().getModel()
-  sap.ui.getCore().attachInit(function () {
-    // Create a text UI element that displays a hardcoded text string
-    new Text({
-      text: "Hi my name is Amir",
-    }).placeAt("content");
-  });
-});
+    // this.getView().getModel() -> another way to define
+    sap.ui.getCore().attachInit(function () {
+      // Create a json model from an object literal
+      // const oModel = new JSONModel({
+      //   greetingText: "Hi my name is Amir from json",
+      // });
+
+      const oModel = new JSONModel();
+      oModel.loadData("./model/test.json");
+
+      sap.ui.getCore().setModel(oModel);
+      // this.getView().setModel(oMOdel)  -> another way to define the model
+
+      // Create a text UI element that displays a hardcoded text string
+      // new Text({
+      //   text: "Hi my name is Amir from text",
+      // }).placeAt("content");
+
+      // Display a Text element whose text is from the model object
+      new Text({
+        text: "{/greetingText}",
+      }).placeAt("content");
+    });
+  }
+);
