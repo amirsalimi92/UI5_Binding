2a9a2932bb026397cccf6947eeca789d543757ff
Amir Salimi <85235736+amirsalimi92@users.noreply.github.com>
2024-05-16 11:28:11 +0200

    Step4_Start with forms

--- webapp/scripts/index.js
+++ webapp/scripts/index.js
@@ -1,8 +1,6 @@
-console.log("test");
-
 sap.ui.require(
-  ["sap/ui/model/json/JSONModel", "sap/m/Text"],
-  function (JSONModel, Text) {
+  ["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/XMLView"],
+  function (JSONModel, XMLView) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
@@ -16,8 +14,8 @@ sap.ui.require(
 
       const oModel = new JSONModel();
       oModel.loadData("./model/test.json");
-
       sap.ui.getCore().setModel(oModel);
+
       // this.getView().setModel(oMOdel)  -> another way to define the model
 
       // Create a text UI element that displays a hardcoded text string
@@ -26,8 +24,13 @@ sap.ui.require(
       // }).placeAt("content");
 
       // Display a Text element whose text is from the model object
-      new Text({
-        text: "{/greetingText}",
+      // new Text({
+      //   text: "{/greetingText}",
+      // }).placeAt("content");
+
+      // Display the XML view which called App.view.xml
+      new XMLView({
+        viewName: "myapp.view.App",
       }).placeAt("content");
     });
   }
