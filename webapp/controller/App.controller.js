5312a0b20e9b72ae359fa0f064ecdbaca2d42566
Amir Salimi <85235736+amirsalimi92@users.noreply.github.com>
2024-05-26 19:53:19 +0200

    Final

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,8 +5,16 @@ sap.ui.define(
     "sap/ui/core/Locale",
     "sap/ui/core/LocaleData",
     "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute",
   ],
-  function (Controller, library, Locale, LocaleData, Currency) {
+  function (
+    Controller,
+    library,
+    Locale,
+    LocaleData,
+    Currency,
+    ObjectAttribute
+  ) {
     "use strict";
 
     return Controller.extend("myapp.controller.App", {
@@ -41,11 +49,38 @@ sap.ui.define(
         const oContext = oSelecteditem.getBindingContext("products");
 
         const sPath = oContext.getPath();
-        console.log(sPath);
         // Set Id from View in Panel
         const oProductDetailPanel = this.byId("productDetailsPanel");
-        console.log(oProductDetailPanel);
         oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+      },
+
+      productListFactory: function (sId, oContext) {
+        let oUIControl;
+
+        // Decide based on the data which dependent to clone
+        if (
+          oContext.getProperty("UnitInStock") === 0 &&
+          oContext.getProperty("Discontinued")
+        ) {
+          // The item is discontinued, so use a StandardListItem
+          oUIControl = this.byId("productSimple").clone(sId);
+        } else {
+          // The item is available, so we will create an ObjectListItem
+          oUIControl = this.byId("productExtended").clone(sId);
+
+          // the item is temporarily out of stock, so we will add a ststus
+          if (oContext.getProperty("UnitsInStock") < 1) {
+            oUIControl.addAttribute(
+              new ObjectAttribute({
+                text: {
+                  path: "i18n>outOfStock",
+                },
+              })
+            );
+          }
+        }
+
+        return oUIControl;
       },
     });
   }
